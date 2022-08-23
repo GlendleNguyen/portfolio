105f7751dca081174ad3080d7a1217049dd154d1
Glen Nguyen <glendle.nguyen@gmail.com>
2022-08-23 21:08:31 +0800

    add components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,13 +2,7 @@ import React, { useState } from 'react';
 import {
     FaBars,
     FaTimes,
-    FaGithub,
-    FaLinkedin,
-    FaFacebook,
-    FaLinkedinIn,
 } from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -34,13 +28,8 @@ const Navbar = () => {
             </Link>
             </li>
             <li>
-            <Link to='skills' smooth={true} duration={500}>
-                Skills
-            </Link>
-            </li>
-            <li>
-            <Link to='work' smooth={true} duration={500}>
-                Work
+            <Link to='projects' smooth={true} duration={500}>
+                Projects
             </Link>
             </li>
             <li>
@@ -76,14 +65,8 @@ const Navbar = () => {
             </li>
             <li className='py-6 text-4xl'>
             {' '}
-            <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
-                Skills
-            </Link>
-            </li>
-            <li className='py-6 text-4xl'>
-            {' '}
-            <Link onClick={handleClick} to='work' smooth={true} duration={500}>
-                Work
+            <Link onClick={handleClick} to='projects' smooth={true} duration={500}>
+                Projects
             </Link>
             </li>
             <li className='py-6 text-4xl'>
@@ -94,43 +77,6 @@ const Navbar = () => {
             </li>
         </ul>
 
-        {/* Social icons */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-            <ul>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                <a
-                className='flex justify-between items-center w-full text-gray-300'
-                href='/'
-                >
-                Linkedin <FaLinkedin size={30} />
-                </a>
-            </li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                <a
-                className='flex justify-between items-center w-full text-gray-300'
-                href='/'
-                >
-                Github <FaGithub size={30} />
-                </a>
-            </li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-                <a
-                className='flex justify-between items-center w-full text-gray-300'
-                href='/'
-                >
-                Email <HiOutlineMail size={30} />
-                </a>
-            </li>
-            <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                <a
-                className='flex justify-between items-center w-full text-gray-300'
-                href='/'
-                >
-                Resume <BsFillPersonLinesFill size={30} />
-                </a>
-            </li>
-            </ul>
-        </div>
         </div>
     );
 };
---
+++ src/components/Projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div name='projects' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        
+        </div>
+    );
+};
+
+export default Projects;
