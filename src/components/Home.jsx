f802d6fc17fd3fd5cc067f50ebe20741d03ca61a
Glen Nguyen <glendle.nguyen@gmail.com>
2022-08-23 20:41:25 +0800

    changed style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,33 @@
 import React from 'react';
-import HeroImage from '../assets/heroImage.jpeg';
-import { MdKeyboardArrowRight } from 'react-icons/md';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
     return (
-        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-
-            <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-                <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Computer Science Student</h2>
-                    <p className='text-gray-500 py-4 max-w-md'>
-                        <p>Hello! I'm a final year CS student at the University of Western Australia. I'm currently active on the Coders for Causes committee. </p>
-                        <br></br>
-                        <p>My current subjects are AI, Secure Coding, and Professional Computing where I am learning and 
-                        using C, Java, Python, Vue, nodeJS, and mongoDB.</p>
-                    </p>
-
-                    <div>
-                            <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer group'>
-                                See More  
-                                <span className='group-hover:rotate-90 duration-300'>
-                                    <MdKeyboardArrowRight size={30} className='ml-1'/>
-                                </span>
-                            </button>
-                    </div>
-                </div>
-
-                <div>
-                    <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full'></img>
-                </div>
-            </div>
-
+        <div name='home' className='w-full h-screen bg-[#0a192f]'>
+        {/* Container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+            Glen Nguyen
+            </h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+            I'm a Computer Science Student.
+            </h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>
+            I’m a final year CS student at the University of Western Australia and I am active on the 
+            Coders for Causes committee. My units this semester are AI, secure coding, and professional computing. 
+            </p>
+            <div>
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+                View Work
+                <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-3 ' />
+                </span>
+            </button>
         </div>
-    )
-}
+        </div>
+    </div>
+    );
+};
 
-export default Home
+export default Home;
